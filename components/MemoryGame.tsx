e1f3c3f22fd89782b60ef26ee6fb9ecd3cddfe97
Ajarek <ajarek@poczta.onet.pl>
2024-11-02 01:22:12 +0100

    added hidden

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import cards from '@/data/dataCards.json'
+import { log } from 'console'
 interface Card {
   id: number
   background: string
@@ -10,11 +11,24 @@ interface Card {
 
 const MemoryGame = () => {
   const [nowCards, setNowCards] = useState<Card[]>([])
+  const [selectCards, setSelectCards] = useState<Card[]>([])
+  const [hidden, setHidden] = useState<number[]>([])
 
   useEffect(() => {
     const shuffled = shuffleCards(cards)
     setNowCards(shuffled)
   }, [])
+  useEffect(() => {
+    if (selectCards.length === 2) {
+      const [firstCard, secondCard] = selectCards;
+      if (firstCard?.background === secondCard?.background) {
+        setHidden(prevHidden => [...prevHidden, firstCard.id, secondCard.id]);
+      }
+    }
+  }, [selectCards])
+
+
+
 
   const shuffleCards = (cards: Card[]) => {
     const shuffled = [...cards]
@@ -26,8 +40,14 @@ const MemoryGame = () => {
   }
 
   const handleCart=(id:number)=>{
+   
+    if(selectCards.length>=2){
+      return
+    }
+    
     const newCards=nowCards.map((card:Card)=>{
       if(card.id===id){
+        setSelectCards([...selectCards, card])
         return{
           ...card,
           isFlipped:!card.isFlipped
@@ -36,24 +56,30 @@ const MemoryGame = () => {
       return card
 
     })
-
     setNowCards(newCards)
+    setTimeout(() => {
+      setSelectCards([])
+      setNowCards(nowCards.map(card => ({...card, isFlipped: false})))
+    }, 5000)
   }
-
+ 
   return (
     <div className='grid grid-cols-4 gap-4'>
       {nowCards.map((card: Card) => (
+        
         <div
           key={card.id}
           className='w-32 h-32 border-2 rounded-lg overflow-hidden'
-          onClick={()=>handleCart(card.id)}
+          onClick={() => handleCart(card.id)}
+          hidden={hidden.includes(card.id)}
         >
           <div className='relative w-full h-full'>
             {card.isFlipped ? (
-               <Image
+              <Image
                src={card.background}
                alt='card'
                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                className='object-cover '
              />
             ) : (
