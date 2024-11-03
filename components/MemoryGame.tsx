a548e04cea2ef0a5fde0ff3a657e9481e75c7bd8
Ajarek <ajarek@poczta.onet.pl>
2024-11-03 23:26:48 +0100

    delete header , timer

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -2,7 +2,10 @@
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import cards from '@/data/dataCards.json'
-import { GameHeader } from '@/components/MemoryGame-header'
+import { Badge } from '@/components/ui/badge'
+import { Sparkles, RotateCcw, Timer } from 'lucide-react'
+import { Button } from './ui/button'
+
 interface Card {
   id: number
   background: string
@@ -14,6 +17,8 @@ const MemoryGame = () => {
   const [selectCards, setSelectCards] = useState<Card[]>([])
   const [hidden, setHidden] = useState<number[]>([])
   const [newGame, setNewGame] = useState<boolean>(false)
+  const [seconds, setSeconds] = useState(0) 
+  const [isActive, setIsActive] = useState(false) 
 
   useEffect(() => {
     const shuffled = shuffleCards(cards)
@@ -21,9 +26,10 @@ const MemoryGame = () => {
   }, [])
 
   useEffect(() => {
-    if (hidden.length === 12) { 
-      setNewGame(true) // Zmieniono na true, aby rozpocząć nową grę
+    if (hidden.length === 12) {
+      setNewGame(true) 
       setHidden([])
+      setIsActive(false)
     }
   }, [hidden])
 
@@ -35,6 +41,18 @@ const MemoryGame = () => {
       }
     }
   }, [selectCards])
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | undefined
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1)
+      }, 1000)
+    } else if (!isActive && seconds !== 0) {
+      if (interval) clearInterval(interval)
+    }
+    return () => interval && clearInterval(interval)
+  }, [isActive, seconds])
 
   const shuffleCards = (cards: Card[]) => {
     const shuffled = [...cards]
@@ -64,20 +82,57 @@ const MemoryGame = () => {
     setTimeout(() => {
       setSelectCards([])
       setNowCards(nowCards.map((card) => ({ ...card, isFlipped: false })))
-    }, 5000)
+    }, 3000)
+  }
+
+  const toggle = () => {
+    setIsActive(!isActive)
+  }
+
+  const reset = () => {
+    setSeconds(0)
+    setIsActive(false)
   }
 
   return (
     <div>
-      <GameHeader handleNewGame={() => setNewGame(true)} />
+      <div className='text-center mb-4'>
+        <h1 className='text-4xl text-yellow-500 font-bold mb-4 flex items-center justify-center gap-2'>
+          <Sparkles className='' />
+          Memory Game
+        </h1>
+        <div className='flex justify-center items-center gap-4 '>
+          <Badge
+            variant='secondary'
+            className='flex items-center gap-2 text-lg px-6 py-[2px]'
+          >
+            <h1>{seconds}s</h1>
+            <Timer className='w-4 h-4 mr-2' />
+          </Badge>
+
+          <Button
+            className=''
+            onClick={() => {
+              setNewGame(true)
+              toggle()
+            }}
+          >
+            <RotateCcw className='w-4 h-4 mr-2' />
+            {isActive ? 'Stop' : 'Start'}
+          </Button>
+          <Button onClick={reset}>Reset</Button>
+        </div>
+      </div>
+
       {newGame && (
         <div className='grid grid-cols-4 gap-4'>
           {nowCards.map((card: Card) => (
             <div
               key={card.id}
-              className='w-32 h-32 border-2 rounded-lg overflow-hidden'
+              className='w-32 h-32 border-2 rounded-lg overflow-hidden cursor-pointer'
               onClick={() => handleCart(card.id)}
               hidden={hidden.includes(card.id)}
+             
             >
               <div className='relative w-full h-full'>
                 {card.isFlipped ? (
@@ -89,7 +144,15 @@ const MemoryGame = () => {
                     className='object-cover '
                   />
                 ) : (
-                  <div className='absolute top-0 left-0 w-full h-full bg-black opacity-50'></div>
+                  <div className='absolute top-0 left-0 w-full h-full  '>
+                     <Image
+                    src='/images/orc-connector.png'
+                    alt='card'
+                    fill
+                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                    className='object-cover bg-amber-400 '
+                  />
+                  </div>
                 )}
               </div>
             </div>
